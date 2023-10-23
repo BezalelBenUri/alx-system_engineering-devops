8e625ca9285f87f6bbced4e3161885defd9cdee6
PhenomenalAI <diamondinyang@gmail.com>
2023-10-23 09:50:58 -0700

    Gather data from a REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Returns to-do data for a given employee ID."""
-import sys
+"""API that Returns to-do list information for a given employee ID."""
 import requests
-
+import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [godt.get("title") for godt in todos if godt.get(
+    completed = [bat.get("title") for bat in todos if bat.get(
         "completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(haa)) for haa in completed]
+    [print("\t {}".format(c)) for c in completed]
