f0a459c35f3d2c73497b211c9611ae87496bd0e6
PhenomenalAI <diamondinyang@gmail.com>
2023-10-23 08:09:53 -0700

    Gather data from a REST API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Returns to-do data for a given employee ID."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [godt.get("title") for godt in todos if godt.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(haa)) for haa in completed]
