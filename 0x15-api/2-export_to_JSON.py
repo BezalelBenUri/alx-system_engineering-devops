87ef933b2a95369a7a1703cbb46dbc35998fee17
PhenomenalAI <diamondinyang@gmail.com>
2023-10-23 09:10:19 -0700

    ...data from api to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Return to-do list data for a given employee ID to JSON"""
+"""Returns to-do list data for a given employee ID to JSON format."""
 import json
 import requests
 import sys
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get(url + "todos", params={"userId": user_id}).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
