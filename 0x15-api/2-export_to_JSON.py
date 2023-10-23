5caf865773cb0ae37af614c0ddd5876408528d5d
PhenomenalAI <diamondinyang@gmail.com>
2023-10-23 10:06:29 -0700

    ...api data to json format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Returns to-do list data for a given employee ID to JSON format."""
+import json
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
